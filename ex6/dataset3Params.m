ac2b357ce693df0da5d39bce98e1d368f9f60d67
Yaning Huang <ynhuang@ynhuang-macbookpro.roam.corp.google.com>
2018-10-19 13:57:01 -0700

    svm

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -10,6 +10,10 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 % You need to return the following variables correctly.
 C = 1;
 sigma = 0.3;
+error_matrix = zeros(8, 8);
+
+super_parameter_candidates = [0.01 0.03 0.1 0.3 1 3 10 30];
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
@@ -23,10 +27,20 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
-
+for i = 1:8;
+	for j = 1:8;
+		temp_C = super_parameter_candidates(i);
+		temp_sigma = super_parameter_candidates(j);
+		model = svmTrain(X, y, temp_C, @(x1, x2) gaussianKernel(x1, x2, temp_sigma));
+		predictions = svmPredict(model, Xval);
+		err = mean(double(predictions ~= yval));
+		error_matrix(i,j) = err;
+	end;
+end;
+[M,I] = min(error_matrix(:));
+[I_row, I_col] = ind2sub(size(error_matrix),I);
+C = super_parameter_candidates(I_row);
+sigma = super_parameter_candidates(I_col);
 
 
 % =========================================================================
